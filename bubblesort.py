5e3134afecee78447f450b6ba4f5d71246a2a0b1
Marie <marie.hwang@gmail.com>
2017-07-09 16:30:35 -0500

    change of file name

---
+++ bubblesort.py
@@ -0,0 +1,15 @@
+/* text file                        */
+/* This line has been added in Atom */
+
+def swap(a, x, y):
+    tmp = a[x]
+    a[x] = a[y]
+    a[y] = tmp
+    return a
+
+def bubble_sort(x):
+    for i in range(len(x)):
+        for k in range(len(x) - 1, i, -1):
+            if x[k] < x[k - 1]:
+                swap(x, k, k - 1)
+    return x
